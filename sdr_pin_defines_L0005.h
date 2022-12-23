e15cf8fbb541581439737b595b9d7bbdecb45e1c
Colton Acosta <cacost12@asu.edu>
2022-12-23 11:08:17 -0700

    added solenoid gpio pin defs

--- sdr_pin_defines_L0005.h
+++ sdr_pin_defines_L0005.h
@@ -34,14 +34,30 @@ extern "C" {
 	#define STATUS_PIN    GPIO_PIN_15
 	#define EXCEPT_PIN    GPIO_PIN_13
 
+	/* Solenoids */
+	#define SOL1_PIN   GPIO_PIN_2
+	#define SOL2_PIN   GPIO_PIN_3
+	#define SOL3_PIN   GPIO_PIN_4
+	#define SOL4_PIN   GPIO_PIN_0
+	#define SOL5_PIN   GPIO_PIN_1
+	#define SOL6_PIN   GPIO_PIN_2
+
 
 	/*-------------------------------------------------------------------------
 	 MCU Port Assignments                                                          
 	--------------------------------------------------------------------------*/
 
 	/* LEDs */
-	#define STATUS_PORT    GPIOA
-	#define EXCEPT_PORT    GPIOD
+	#define STATUS_GPIO_PORT    GPIOA
+	#define EXCEPT_GPIO_PORT    GPIOD
+
+	/* Solenoids */
+	#define SOL1_GPIO_PORT   GPIOE
+	#define SOL2_GPIO_PORT   GPIOE
+	#define SOL3_GPIO_PORT   GPIOE
+	#define SOL4_GPIO_PORT   GPIOA
+	#define SOL5_GPIO_PORT   GPIOA
+	#define SOL6_GPIO_PORT   GPIOA
 
 
 	/*--------------------------------------------------------------------------
