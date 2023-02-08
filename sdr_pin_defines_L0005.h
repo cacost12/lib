3e83e810e15c37ec7ab642c321c7e1b5226a2bef
Colton Acosta <cacost12@asu.edu>
2023-02-08 16:08:29 -0700

    added lox encoder pins to valve controller pin defines

--- sdr_pin_defines_L0005.h
+++ sdr_pin_defines_L0005.h
@@ -31,16 +31,20 @@ extern "C" {
 	--------------------------------------------------------------------------*/
 
 	/* LEDs */
-	#define STATUS_PIN    GPIO_PIN_15
-	#define EXCEPT_PIN    GPIO_PIN_13
+	#define STATUS_PIN           GPIO_PIN_15
+	#define EXCEPT_PIN           GPIO_PIN_13
 
 	/* Solenoids */
-	#define SOL1_PIN   GPIO_PIN_2
-	#define SOL2_PIN   GPIO_PIN_3
-	#define SOL3_PIN   GPIO_PIN_4
-	#define SOL4_PIN   GPIO_PIN_0
-	#define SOL5_PIN   GPIO_PIN_1
-	#define SOL6_PIN   GPIO_PIN_2
+	#define SOL1_PIN             GPIO_PIN_2
+	#define SOL2_PIN             GPIO_PIN_3
+	#define SOL3_PIN             GPIO_PIN_4
+	#define SOL4_PIN             GPIO_PIN_0
+	#define SOL5_PIN             GPIO_PIN_1
+	#define SOL6_PIN             GPIO_PIN_2
+
+	/* Servo Valves */
+	#define LOX_ENC_A_PIN        GPIO_PIN_8
+	#define LOX_ENC_B_PIN        GPIO_PIN_9
 
 
 	/*-------------------------------------------------------------------------
@@ -48,16 +52,19 @@ extern "C" {
 	--------------------------------------------------------------------------*/
 
 	/* LEDs */
-	#define STATUS_GPIO_PORT    GPIOA
-	#define EXCEPT_GPIO_PORT    GPIOD
+	#define STATUS_GPIO_PORT     GPIOA
+	#define EXCEPT_GPIO_PORT     GPIOD
 
 	/* Solenoids */
-	#define SOL1_GPIO_PORT   GPIOE
-	#define SOL2_GPIO_PORT   GPIOE
-	#define SOL3_GPIO_PORT   GPIOE
-	#define SOL4_GPIO_PORT   GPIOA
-	#define SOL5_GPIO_PORT   GPIOA
-	#define SOL6_GPIO_PORT   GPIOA
+	#define SOL1_GPIO_PORT       GPIOE
+	#define SOL2_GPIO_PORT       GPIOE
+	#define SOL3_GPIO_PORT       GPIOE
+	#define SOL4_GPIO_PORT       GPIOA
+	#define SOL5_GPIO_PORT       GPIOA
+	#define SOL6_GPIO_PORT       GPIOA
+	
+	/* Servo Valves */
+	#define LOX_ENC_GPIO_PORT    GPIOC
 
 
 	/*--------------------------------------------------------------------------
