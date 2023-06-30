bb528ad3e06c0d4f77b3021b03e518689ff02217
Colton Acosta <cacost12@asu.edu>
2023-06-30 07:01:13 -0700

    removed sdr pin defines files and renamed new files

--- pin_defines_A0002.h
+++ pin_defines_A0002.h
@@ -1,10 +1,10 @@
 /*******************************************************************************
 *                                                                              *
 * FILE:                                                                        * 
-* 		sdr_pin_defines_A0007_rev1.h                                           *
+* 		sdr_pin_defines_A0002_rev1.h                                           *
 *                                                                              *
 * DESCRIPTION:                                                                 * 
-* 	    Contains all the MCU pin definitions for the flight computer lite      *
+* 	    Contains all the MCU pin definitions for flight computer               *
 *                                                                              *
 *******************************************************************************/
 
@@ -45,16 +45,35 @@ Includes
 #define FLASH_SCK_PIN             GPIO_PIN_13
 #define FLASH_MISO_PIN            GPIO_PIN_14
 #define FLASH_MOSI_PIN            GPIO_PIN_15
-#define FLASH_WP_PIN              GPIO_PIN_13
-#define FLASH_HOLD_PIN            GPIO_PIN_12
+#define FLASH_WP_PIN              GPIO_PIN_12
+#define FLASH_HOLD_PIN            GPIO_PIN_13
+
+/* SD Card */
+#define SDR_SD_DETECT_PIN         GPIO_PIN_15
 
 /* Baro Pressure Sensor */
 #define BP_INT_PIN                GPIO_PIN_3
 
+/* IMU */
+#if   defined( A0002_REV1 )
+	#define IMU_INT_PIN           GPIO_PIN_2
+#elif defined( A0002_REV2 )
+	#define IMU_INT1_PIN          GPIO_PIN_2
+	#define IMU_INT2_PIN          GPIO_PIN_2
+#endif
+
 /* USB */
 #define USB_SUSPEND_PIN           GPIO_PIN_8
 #define USB_RST_PIN               GPIO_PIN_12
-#define USB_DETECT_PIN            GPIO_PIN_9
+#ifdef A0002_REV2
+	#define USB_DETECT_PIN        GPIO_PIN_9
+#endif
+
+/* Magnetometer */
+#ifdef A0002_REV2
+	#define MAG_INT_PIN           GPIO_PIN_7
+	#define MAG_DRDY_PIN          GPIO_PIN_2
+#endif
 
 
 /*-----------------------------------------------------------------------------
@@ -66,7 +85,7 @@ Includes
 
 /* Ignition */
 #define SWITCH_GPIO_PORT          GPIOD
-#define MAIN_GPIO_PORT            GPIOE
+#define MAIN_GPIO_PORT            GPIOD
 #define MAIN_CONT_GPIO_PORT       GPIOE
 #define DROGUE_GPIO_PORT          GPIOC
 #define DROGUE_CONT_GPIO_PORT     GPIOC
@@ -79,13 +98,32 @@ Includes
 #define FLASH_WP_GPIO_PORT        GPIOD
 #define FLASH_HOLD_GPIO_PORT      GPIOD
 
+/* SD Card */
+#define SDR_SD_DETECT_GPIO_PORT   GPIOD
+
 /* Baro Pressure Sensor */
 #define BP_INT_GPIO_PORT          GPIOE
+
+/* IMU */
+#if   defined( A0002_REV1 )
+	#define IMU_INT_GPIO_PORT     GPIOA 
+#elif defined( A0002_REV2 )
+	#define IMU_INT1_GPIO_PORT    GPIOC 
+	#define IMU_INT2_GPIO_PORT    GPIOA 
+#endif
 
 /* USB */
 #define USB_SUSPEND_GPIO_PORT     GPIOA 
 #define USB_RST_GPIO_PORT         GPIOA 
-#define USB_DETECT_GPIO_PORT      GPIOD 
+#ifdef A0002_REV2
+	#define USB_DETECT_GPIO_PORT  GPIOD 
+#endif
+
+/* Magnetometer */
+#ifdef A0002_REV2
+	#define MAG_INT_GPIO_PORT     GPIOE
+	#define MAG_DRDY_GPIO_PORT    GPIOB 
+#endif
 
 
 /*--------------------------------------------------------------------------
@@ -94,6 +132,8 @@ Includes
 
 /* MCU Peripheral Handles */
 extern I2C_HandleTypeDef  hi2c1;  /* Baro I2C     */
+extern I2C_HandleTypeDef  hi2c2;  /* IMU I2C      */
+extern SD_HandleTypeDef   hsd1;   /* SD Card      */
 extern SPI_HandleTypeDef  hspi2;  /* Flash SPI    */
 extern TIM_HandleTypeDef  htim4;  /* Buzzer Timer */
 extern UART_HandleTypeDef huart6; /* USB UART     */
@@ -101,8 +141,10 @@ extern UART_HandleTypeDef huart6; /* USB UART     */
 /* Peripheral Compatibility Macros */
 #define BARO_I2C                  hi2c1 
 #define FLASH_SPI                 hspi2
+#define IMU_I2C                   hi2c2
 #define USB_HUART                 huart6
 #define BUZZ_TIM                  htim4
+#define SD_HSD                    hsd1
 
 /* Timer channels */
 #define BUZZ_TIM_CHANNEL         TIM_CHANNEL_3
