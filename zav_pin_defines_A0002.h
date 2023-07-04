6b637c0b41bf60ace0864bee73dda8067c819159
Colton Acosta <cacost12@asu.edu>
2023-07-04 14:32:08 -0700

    added barometric pressure sensor mcu i2c initialization to full flight computer pin defines

--- zav_pin_defines_A0002.h
+++ zav_pin_defines_A0002.h
@@ -158,10 +158,12 @@ Includes
 --------------------------------------------------------------------------*/
 
 /* MCU Peripheral handles for module access */
-extern SPI_HandleTypeDef  flash_hspi;  /* Flash SPI */
-extern UART_HandleTypeDef usb_huart;   /* USB UART  */
+extern I2C_HandleTypeDef  baro_hi2c;   /* Baro Sensor I2C */
+extern SPI_HandleTypeDef  flash_hspi;  /* Flash SPI       */
+extern UART_HandleTypeDef usb_huart;   /* USB UART        */
 
 /* Compatibility Macros, defined for hardware independent APIs */
+#define BARO_I2C          I2C1
 #define FLASH_SPI         SPI2 
 #define USB_UART          USART6
 
